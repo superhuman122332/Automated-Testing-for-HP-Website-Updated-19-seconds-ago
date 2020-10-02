f9857015b925a21ad6b2e38c5ec4a094c762525f
superhuman122332 <72231812+superhuman122332@users.noreply.github.com>
2020-10-02 19:55:41 -0400

    Create Feature Verification.java

---
+++ Ideas/Feature Verification.java
@@ -0,0 +1,67 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.io.IOExceptions
+import java.io
+import java.util.hashmap 
+
+ 
+
+public class main{
+
+interface type <T>{
+    Webdriver String (int myURL);
+    myUrl identifier = "https";  
+        
+    public static void main(String[] args)
+    throws IOExceptions   
+    {
+        Webdriver driver = new Firefox driver;
+    
+        Webdriverwait wait = new WebdriverWait( driver, Duration.ofSeconds(10));
+        
+        while (Webdriver != null)
+      
+        try {
+        
+        driver.get("https://globalnav-pro.hp.com/us/en/globalhf.html"); 
+        driver.getElement(By.name("_Ox42bd")).sendKeys("0x4ebd" + keys.Enter);
+        System.out.Println($miniCart Container)
+        
+        driver.get("https://globalnav-pro.hp.com/us/en/globalhf.html
+        driver.getElement(By.name("Search HP.com")).sendKeys("test" + keys.Enter);
+        System.out.Println(search result)
+              
+        driver.get("https://globalnav-pro.hp.com/us/en/globalhf.html/xmlsitemap")
+        Webdriver deliver.actuate.openElement(By.name("Eventlistener")).sendKeys("EventPropogation" + keys.Enter);
+        System.out.Println(country, language) 
+        
+                       
+    } 
+    if (all && redirect (isTrue().store while (store = "hp.store.com"))) 
+             
+        throw new(Country, Language)  
+        
+        String.Format ["https://globalnav-pro.hp.com/country/language/globalhf.html"]
+        
+        Search Start Search = searchbar.exactsearch(Constants.Start)
+        
+            if (Region == Africa)
+            
+                this.language = Afrikkan
+                this.language.next = Afrique       
+                
+            if(Region == America)
+                
+                this.language = English
+              
+                this.language.next = new(language().new.region()available)
+   }
+    
+ } 
+
+}
